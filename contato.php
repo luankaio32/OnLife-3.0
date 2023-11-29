6c665818bee53438fdfb0b339ceee97fdf2d2ff2
luankaio32 <144280159+luankaio32@users.noreply.github.com>
2023-11-29 08:07:54 -0300

    Add files via upload

---
+++ contato.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nosso Contato</title>
+  <link rel="stylesheet" href="contato.css">
+</head>
+<body>
+
+<a href="landing.php" class="back-button">Voltar</a>
+
+  <div class="intro-container">
+    <img src="Ativo_7-8.png" alt="Logo da Sua Empresa" class="logo">
+    <div class="intro-text">
+      <p>Sejam bem-vindos à área de contatos. No momento, estamos recebendo sugestões e reclamações pelo email. Por favor, preencha o formulário abaixo e nos envie. Respondemos em até 24 horas.</p>
+    </div>
+  </div>
+
+  <form action="https://formspree.io/f/meqbbkrr" method="POST">
+    <input type="hidden" name="_replyto" value="kevin.araujo.mesquita@gmail.com">
+    <label for="name">Nome:</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="email">E-mail:</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="message">Mensagem:</label>
+    <textarea id="message" name="message" rows="4" required></textarea>
+
+    <input type="submit" value="Enviar">
+  </form>
+<script>
+if <form action="true" method="post"> <a href="landing.php"></a></form>
+</script>
+</body>
+</html>
