0b5c5c3fe7970566fd6fe8186e2b7035ab1714f9
luankaio32 <144280159+luankaio32@users.noreply.github.com>
2023-11-29 08:09:40 -0300

    Add files via upload

---
+++ finalizada.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compra Finalizada</title>
+    <link rel="stylesheet" href="finalizada.css">
+</head>
+<body>
+    <div id="container">
+        <img id="logo" src="iconePrincipal.png" alt="Logo da Empresa">
+        <div id="mensagem">Compra Finalizada com Sucesso!</div>
+        <div id="botoes">
+            <a class="botao" id="paginaCompra" href="compra.php">Voltar à Página de Compra</a>
+            <a class="botao" id="paginaInicio" href="landing.php">Voltar à Página Inicial</a>
+        </div>
+    </div>
+</body>
+</html>
