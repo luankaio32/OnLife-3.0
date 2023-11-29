8001f0f0517033051299fcab92a94cb436ba8f48
luankaio32 <144280159+luankaio32@users.noreply.github.com>
2023-11-29 08:15:10 -0300

    Add files via upload

---
+++ sobre.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sobre Nós</title>
+  <link rel="stylesheet" href="sobre.css">
+</head>
+<body>
+
+  <header>
+    <div class="logo-container">
+      <img src="iconeBranco.png" alt="Logo da Sua Empresa">
+    </div>
+    <a href="landing.php" class="back-btn">&#9665; Voltar</a>
+  </header>
+
+  <section class="about-us">
+    <div class="content">
+      <h2>Nosso Objetivo</h2>
+      <p>O Onlife foi criado atentando-se para a dificuldade atual de pessoas de idade ou com deficiências conseguirem seus medicamentos, em razão da dificuldade para chegarem a uma farmácia ou centro médico. Apresentamos a solução desses e outros usuários poderem utilizar nossos serviços para encontrar os seus medicamentos com o melhor custo benefício e no conforto de suas casas.</p>
+    </div>
+  </section>
+
+  <section class="team">
+    <div class="content">
+      <h2>Nossa Equipe</h2>
+      <p>Nossa equipe é composta por profissionais dedicados e apaixonados por fazer a diferença na vida das pessoas. Juntos, trabalhamos para fornecer soluções inovadoras e acessíveis para o setor de saúde.</p>
+    </div>
+  </section>
+
+  <section class="values">
+    <div class="content">
+      <h2>Nossos Valores</h2>
+      <p>Nosso compromisso é baseado em valores fundamentais, como integridade, empatia e excelência. Buscamos constantemente superar as expectativas e proporcionar experiências positivas para nossos usuários.</p>
+    </div>
+  </section>
+
+  <section class="contact">
+    <div class="content">
+      <h2>Entre em Contato</h2>
+      <p>Estamos aqui para ajudar. Se você tiver alguma dúvida, sugestão ou comentário, não hesite em entrar em contato conosco. Sua opinião é valiosa para nós.</p>
+      <p>Email: contato@onlife.com</p>
+      <p>Telefone: (XX) XXXX-XXXX</p>
+    </div>
+  </section>
+
+  <footer>
+    <p>&copy; 2023 Onlife - Todos os direitos reservados</p>
+  </footer>
+
+</body>
+</html>
